115333af9b49ea896cd9ff2069edc37a23ac5642
samakba <sam.akbarian@mail.utoronto.ca>
2025-05-14 16:12:16 -0400

    some few fixes

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,11 +7,11 @@ class FizzBuzz {
 
         for (int i = 1; i < 100; i++) {
 
-            DoFizzBuzz(i);
+            extracted(i);
         }
     }
 
-    private static void DoFizzBuzz(int i) {
+    private static void extracted(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
